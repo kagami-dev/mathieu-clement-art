c03b000fe817179ac29567035d7a69729f562052
castano cindy <castano.cindy1@gmail.com>
2021-11-25 19:04:37 +0100

    fonts and colors setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,31 @@ module.exports = {
       center: true,
     },
     extend: {
-      colors: {},
+      colors: {
+        lightGray: {
+          DEFAULT: '#F0EFEA'
+        },
+        darkYellow: {
+          DEFAULT: '#322E23'
+        },
+        white: {
+          DEFAULT: '#ffffff'
+        },
+        bodyText: {
+          DEFAULT: '#4D4A45'
+        },
+        gold: {
+          DEFAULT: '#B0A890'
+        },
+        blue: {
+          DEFAULT: '#6389C2'
+        }
+      },
     },
+    fontFamily: {
+      'sans': ['Inter', 'sans-serif'],
+      'serif': ['El Messiri', 'serif']
+     }
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
